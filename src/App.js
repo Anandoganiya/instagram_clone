80e94b69fe89bf93ea218ed7549ff9915bf67281
Anandoganiya <Anandoganiya@gmail.com>
2022-06-01 23:18:33 +0530

    added protected routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import {BrowserRouter as Router,Routes,Route}  from 'react-router-dom';
 import {DashBoard,Profile,SignUP,LogIn} from './pages';
 import { useAuth } from './hooks/user-auth';
 import userContext from './context/userContext';
+import ProtectedRoute from './helper/ProtectedRoute';
 function App() {
   const {user} = useAuth();
   return (
       <userContext.Provider value={{user}}>
         <Router>
           <Routes>
+            <Route path='/' element={<ProtectedRoute user={user}/>}>
               <Route path='/' element={<DashBoard/>}></Route>
+            </Route>
               <Route path='/login' element={<LogIn/>}></Route>
               <Route path='/signup' element={<SignUP/>}></Route>
               <Route path='/p/:username' element={<Profile/>}></Route>
