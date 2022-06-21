83d3e679ef87742b5858504dcf6b6d59816ba707
Anandoganiya <Anandoganiya@gmail.com>
2022-06-21 20:54:35 +0530

    added loading spinner to profile header:

--- src/components/post/AddComment.jsx
+++ src/components/post/AddComment.jsx
@@ -1,29 +1,48 @@
-import {useState} from 'react'
-import useUser from '../../hooks/user-info'
-import { arrayUnion,updateDoc,doc, } from 'firebase/firestore'
-import {db} from '../../lib/firebaseConfig'
-const AddComment = ({commentInput,comment,setComment,docId}) => {
-  const [inputComment,setInputComment] = useState('')
-  const {user:{username}} = useUser()
-  const handleCommentForm = async(e) => {
-    e.preventDefault()
-    setComment([{displayName:username,comment:inputComment},...comment])
-    setInputComment('')
-    const docRef = doc(db, 'photos', docId)
-    await updateDoc(docRef,{
-        comments:arrayUnion({displayName:username,comment:inputComment})
-    })
-}
+import { useState } from "react";
+import useUser from "../../hooks/user-info";
+import { arrayUnion, updateDoc, doc } from "firebase/firestore";
+import { db } from "../../lib/firebaseConfig";
+const AddComment = ({ commentInput, comment, setComment, docId }) => {
+  const [inputComment, setInputComment] = useState("");
+  const {
+    user: { username },
+  } = useUser();
+  const handleCommentForm = async (e) => {
+    e.preventDefault();
+    setComment([{ displayName: username, comment: inputComment }, ...comment]);
+    setInputComment("");
+    const docRef = doc(db, "photos", docId);
+    await updateDoc(docRef, {
+      comments: arrayUnion({ displayName: username, comment: inputComment }),
+    });
+  };
+
   return (
     <div className="w-full">
-        <form action="" onSubmit={(e)=>inputComment.length >= 1?handleCommentForm(e):e.preventDefault()} className="w-full flex border justify-between">
-            <input type="text" ref={commentInput} value={inputComment} placeholder='Add a comment'
-             onChange={({target})=>setInputComment(target.value)}
-             className='w-full outline-none p-1'/>
-            <button disabled={inputComment.length < 1} className="w-[5rem] bg-gray-100">submit</button>
-        </form>
+      <form
+        action=""
+        onSubmit={(e) =>
+          inputComment.length >= 1 ? handleCommentForm(e) : e.preventDefault()
+        }
+        className="w-full flex border justify-between"
+      >
+        <input
+          type="text"
+          ref={commentInput}
+          value={inputComment}
+          placeholder="Add a comment"
+          onChange={({ target }) => setInputComment(target.value)}
+          className="w-full outline-none p-1"
+        />
+        <button
+          disabled={inputComment.length < 1}
+          className="w-[5rem] bg-gray-100"
+        >
+          submit
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddComment
+export default AddComment;
--- src/components/post/Comments.jsx
+++ src/components/post/Comments.jsx
@@ -1,30 +1,47 @@
-import React,{useState} from 'react'
-import { Link } from 'react-router-dom'
-import { formatDistance } from 'date-fns'
-import  AddComment from './AddComment'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { formatDistance } from "date-fns";
+import AddComment from "./AddComment";
 
-const Comments = ({docId,dateCreated,comments,commentInput}) => {
-  const [comment,setComment] = useState(comments)
+const Comments = ({ docId, dateCreated, comments, commentInput }) => {
+  const [comment, setComment] = useState(comments);
+  const [commentsSlice, setCommentsSlice] = useState(3);
+  const showNextComments = () => {
+    setCommentsSlice(commentsSlice + 3);
+  };
   return (
     <div>
-        {
-            comments.slice(0,3).map((item,index)=>(
-                <div className='flex space-x-3 m-1' key={index}>
-                <Link to={`/p/${item.displayName}`}>
-                    <p className='font-semibold'>{item.displayName}</p>
-                </Link>
-                <p>{item.comment}</p>
-                </div>
-            ))
-        }
-        {
-            comments.length  >= 3 &&
-            <p className='cursor-pointer'>View all {comments.length} Comments</p>
-        }
-        <div className='m-1'>{formatDistance(dateCreated,new Date())} ago</div>
-        <AddComment docId={docId} commentInput={commentInput} comment={comment} setComment={setComment}/>
+      {comments.slice(0, 3).map((item, index) => (
+        <div className="flex space-x-3 m-1" key={index}>
+          <Link to={`/p/${item.displayName}`}>
+            <p className="font-semibold">{item.displayName}</p>
+          </Link>
+          <p>{item.comment}</p>
+        </div>
+      ))}
+      {comments.length >= 3 && commentsSlice < comments.length && (
+        <button
+          className="text-sm text-gray-base mb-1 cursor-pointer focus:outline-none"
+          type="button"
+          onClick={showNextComments}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              showNextComments();
+            }
+          }}
+        >
+          View more comments
+        </button>
+      )}
+      <div className="m-1">{formatDistance(dateCreated, new Date())} ago</div>
+      <AddComment
+        docId={docId}
+        commentInput={commentInput}
+        comment={comment}
+        setComment={setComment}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
